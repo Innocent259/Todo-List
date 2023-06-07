111a49c6bca94706e261ed7a143cdd061a1afb8c
innocent259 <ndahimanainnocent842@gmail.com>
2023-06-07 23:24:45 +0200

    Resolve linters errors

--- src/display.js
+++ src/display.js
@@ -1,14 +1,14 @@
 import {
-    getTodoList, markCompleted, removeItem, editItem,
-  } from './todoList.js';
-  
-  const listContainer = document.querySelector('.list');
-  const displayToDoList = (todoList) => {
-    listContainer.innerHTML = '';
-    todoList.forEach((item, index) => {
-      const listItem = document.createElement('div');
-      listItem.className = 'list-properties';
-      listItem.innerHTML = `
+  getTodoList, markCompleted, removeItem, editItem,
+} from './todoList.js';
+
+const listContainer = document.querySelector('.list');
+const displayToDoList = (todoList) => {
+  listContainer.innerHTML = '';
+  todoList.forEach((item, index) => {
+    const listItem = document.createElement('div');
+    listItem.className = 'list-properties';
+    listItem.innerHTML = `
           <div class="both-sides">
             <span class="left-items">
               <input type="checkbox" ${item.completed ? 'checked' : ''} data-index="${index}">
@@ -21,38 +21,37 @@ import {
           </div>
           <hr class="line-1">
         `;
-      const checkBox = listItem.querySelector('input[type="checkbox"]');
-      checkBox.addEventListener('change', (event) => {
-        const index = event.target.getAttribute('data-index');
-        markCompleted(index);
-        displayToDoList(getTodoList());
-      });
-      const description = listItem.querySelector('input[type="text"]');
-      description.addEventListener('input', (event) => {
-        const index = event.target.parentNode.parentNode.querySelector('input[type="checkbox"]').getAttribute('data-index');
-        editItem(index, event.target.value);
-      });
-      description.addEventListener('blur', (event) => {
-        const index = event.target.parentNode.parentNode.querySelector('input[type="checkbox"]').getAttribute('data-index');
-        editItem(index, event.target.value);
-        description.setAttribute('readonly', true);
-        displayToDoList(getTodoList());
-      });
-      const editButton = listItem.querySelector('.edit-item');
-      editButton.addEventListener('click', (event) => {
-        const index = event.target.getAttribute('data-index');
-        const descriptionInput = listItem.querySelector('input[type="text"]');
-        descriptionInput[index].removeAttribute('readonly');
-        descriptionInput[index].focus();
-      });
-      const removeButton = listItem.querySelector('.remove-item');
-      removeButton.addEventListener('click', (event) => {
-        const index = event.target.getAttribute('data-index');
-        removeItem(index);
-        displayToDoList(getTodoList());
-      });
-      listContainer.appendChild(listItem);
+    const checkBox = listItem.querySelector('input[type="checkbox"]');
+    checkBox.addEventListener('change', (event) => {
+      const index = event.target.getAttribute('data-index');
+      markCompleted(index);
+      displayToDoList(getTodoList());
     });
-  };
-  export default displayToDoList;
-  
+    const description = listItem.querySelector('input[type="text"]');
+    description.addEventListener('input', (event) => {
+      const index = event.target.parentNode.parentNode.querySelector('input[type="checkbox"]').getAttribute('data-index');
+      editItem(index, event.target.value);
+    });
+    description.addEventListener('blur', (event) => {
+      const index = event.target.parentNode.parentNode.querySelector('input[type="checkbox"]').getAttribute('data-index');
+      editItem(index, event.target.value);
+      description.setAttribute('readonly', true);
+      displayToDoList(getTodoList());
+    });
+    const editButton = listItem.querySelector('.edit-item');
+    editButton.addEventListener('click', (event) => {
+      const index = event.target.getAttribute('data-index');
+      const descriptionInput = listItem.querySelector('input[type="text"]');
+      descriptionInput[index].removeAttribute('readonly');
+      descriptionInput[index].focus();
+    });
+    const removeButton = listItem.querySelector('.remove-item');
+    removeButton.addEventListener('click', (event) => {
+      const index = event.target.getAttribute('data-index');
+      removeItem(index);
+      displayToDoList(getTodoList());
+    });
+    listContainer.appendChild(listItem);
+  });
+};
+export default displayToDoList;
